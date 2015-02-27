06924c53436ebc495abb12468963c06028b16c58
Emily Saunders Walmsley <EmSaundWalm@gmail.com>
2015-02-27 16:09:32 +1100

    Refactored code, so that common methods are not duplicated across classes

---
+++ src/main/java/org/cytoscape/slimscape/internal/CommonMethods.java
@@ -0,0 +1,294 @@
+package org.cytoscape.slimscape.internal;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonMethods {
+
+    /**
+     * @desc attains and analyses main results from the Slimsuite server.
+     * @param url - The url where the results should be located.
+     * @return Map - A map of List<String> containing the contents of the csv and occ blocks.
+     *               If there is an error, null is returned.
+     */
+    public static List<String> PrepareResults(String url) {
+        // Gets URL data
+        try {
+            URL website = new URL(url);
+            URLConnection connection = website.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+
+            String inputLine;
+            String lineOne = in.readLine();
+
+            // There is an error in the results obtained
+            if (lineOne.startsWith("ERROR")) {
+                in.close();
+                return null;
+            } else {
+                List<String> csv = new ArrayList<String>();
+                csv.add(lineOne);
+                while ((inputLine = in.readLine()) != null) {
+                    StringBuilder builder = new StringBuilder();
+                    boolean inBraces = false;
+                    for (char x : inputLine.toCharArray()) {
+                        if (x == '{') {
+                            inBraces = true;
+                            builder.append(x);
+                        } else if (x == '}') {
+                            inBraces = false;
+                            builder.append(x);
+                        } else if (x == ',' && inBraces) {
+                            builder.append('|');
+                        } else {
+                            builder.append(x);
+                        }
+                    }
+
+                    String toReturn = builder.toString();
+                    csv.add(toReturn);
+                }
+                in.close();
+
+                return csv;
+            }
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    /**
+     * @desc - Gets the IDs of each node in the slimdb results page.
+     * @param url - the URL to search for results in
+     * @return - list of all the IDs found, as strings
+     */
+    public static List<String> getNodeIds(String url) {
+
+        try {
+            URL website = new URL(url);
+            URLConnection connection = website.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+
+            String inputLine;
+
+            List<String> nodeIds = new ArrayList<String>();
+            // Add each line with more than two connections
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.startsWith(">")) {
+                    String[] line = inputLine.split(" ");
+                    nodeIds.add(line[0].substring(1));
+                }
+            }
+            in.close();
+            return nodeIds;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex + " in function");
+            return null;
+        }
+
+    }
+
+    /**
+     * @desc attains and analyses the upc results from the Slimsuite server, so that graph edges can be added later.
+     * @param url - The url where the results should be located.
+     * @return Map - A map of List<String> containing the contents of the upc lines with >1 elements present.
+     *               If there is an error, null is returned.
+     */
+    public static List<String> getUpcResults(String url) {
+
+        try {
+            URL website = new URL(url);
+            URLConnection connection = website.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+
+            String inputLine;
+            String lineOne = in.readLine();
+
+            // There is an error in the results obtained
+            if (lineOne.startsWith("ERROR")) {
+                in.close();
+                return null;
+            } else {
+                in.readLine(); // Removes the second line which contains unnecessary data for our purposes
+                List<String> upc = new ArrayList<String>();
+                // Add each line with more than two connections
+                while ((inputLine = in.readLine()) != null) {
+                    String[] line = inputLine.split("\\t");
+                    if (Integer.parseInt(line[1]) > 1) {
+                        upc.add(line[3]);
+                    }
+                }
+                in.close();
+
+                return upc;
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex + " in function");
+            return null;
+        }
+    }
+
+    /**
+     * @desc Creates a csv-specific JTable from an input of comma separated strings.
+     * @param input - a List<String> consisting of a series of comma-separated lines.
+     * @return JTable - a table populated with the input elements.
+     */
+    public static JTable createCsvTable (List<String> input) {
+        // Get column names from input
+        JTable table;
+        List<String> names = Arrays.asList(input.get(0).split(","));
+        List<String> abbreviated = names.subList(10, names.size()-5);
+        Object columnNames[] = new String[abbreviated.size()];
+        abbreviated.toArray(columnNames);
+
+        // Create table
+        DefaultTableModel model = new DefaultTableModel(null, columnNames);
+        table = new JTable(model);
+        table.setPreferredScrollableViewportSize(new Dimension(400, 700));
+        table.setFillsViewportHeight(true);
+
+        // Add a row in table for each element in the input
+        int lines = input.size();
+        for(int c=1; c<lines; c++) {
+            List<String> line = Arrays.asList(input.get(c).split(","));
+            List<String> abbreviate = line.subList(10, line.size()-5);
+            Object lineObject[] = new String[abbreviate.size()];
+            abbreviate.toArray(lineObject);
+            model.addRow(lineObject);
+        }
+
+        // Table formatting
+        table.setEnabled(false);
+        TableColumn column;
+        for (int i = 0; i < abbreviated.size(); i++) {
+            column = table.getColumnModel().getColumn(i);
+            if (i == 2) {
+                column.setMinWidth(100);
+            } else {
+                column.setMinWidth(50);
+            }
+        }
+        return table;
+    }
+
+    /**
+     * @desc Creates an occ-specific JTable from an input of comma separated strings.
+     * @param input - a List<String> consisting of a series of comma-separated lines.
+     * @return JTable - a table populated with the input elements.
+     */
+    public static JTable createOccTable (List<String> input) {
+        JTable table;
+        List<String> names = Arrays.asList(input.get(0).split(","));
+        List<String> abbreviated = names.subList(3, names.size()-7);
+        Object columnNames[] = new String[abbreviated.size()]; // line 2
+        abbreviated.toArray(columnNames);
+
+        // Create table
+        DefaultTableModel model = new DefaultTableModel(null, columnNames);
+        table = new JTable(model);
+        table.setPreferredScrollableViewportSize(new Dimension(400, 700));
+        table.setFillsViewportHeight(true);
+
+        // Add a row in table for each element in the input
+        int lines = input.size();
+        for(int c=1; c<lines; c++) {
+            List<String> line = Arrays.asList(input.get(c).split(","));
+            List<String> abbreviate = line.subList(3, line.size()-7);
+            Object lineObject[] = new String[abbreviate.size()];
+            abbreviate.toArray(lineObject);
+            model.addRow(lineObject);
+        }
+
+        // Table formatting
+        table.setEnabled(false);
+        TableColumn column;
+        for (int i = 0; i < abbreviated.size(); i++) {
+            column = table.getColumnModel().getColumn(i);
+            column.setMinWidth(100);
+        }
+
+        return table;
+    }
+
+    /**
+     * @desc - gets the job id from a running SLiMSuite service, for later use.
+     * @param url - the URL to search for the ID in.
+     * @return - the ID, as a string.
+     */
+    public static String getJobID(String url) {
+        try {
+            URL website = new URL(url);
+            URLConnection connection = website.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+
+            String inputLine;
+            String lineOne = in.readLine();
+
+            // There is an error in the results obtained
+            if (lineOne.startsWith("ERROR")) {
+                in.close();
+                return null;
+            } else {
+                String id = null;
+                while ((inputLine = in.readLine()) != null) {
+                    if (inputLine.contains("### slimprob job")) {
+                        id = inputLine.split("job")[1];
+                    }
+                }
+                in.close();
+                return id;
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex + " in function");
+            return null;
+        }
+    }
+
+    /**
+     * @desc - Determines whether the job is ready for analysis or still running
+     * @param url - the URL of the run
+     */
+    public static boolean jobReady (String url) {
+        String lineOne;
+        try {
+            URL website = new URL(url);
+            URLConnection connection = website.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+            lineOne = in.readLine();
+            if (lineOne.contains("Finished")) {
+                in.close();
+                return true;
+            } else {
+                in.close();
+                int option = JOptionPane.showConfirmDialog(null, "Run is currently: " + lineOne +
+                        ". Click Yes to check again, or No to stop checking. Please note you'll get errors if you try to"
+                        + "process this before the job is completed.", "Job Pending", JOptionPane.YES_NO_OPTION);
+                return option != JOptionPane.YES_OPTION;
+            }
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+            return false;
+        }
+    }
+
+}
--- src/main/java/org/cytoscape/slimscape/internal/ui/SlimfinderRunPanel.java
+++ src/main/java/org/cytoscape/slimscape/internal/ui/SlimfinderRunPanel.java
@@ -4,6 +4,7 @@ import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.model.*;
 import org.cytoscape.slimscape.internal.AlterGraph;
+import org.cytoscape.slimscape.internal.CommonMethods;
 import org.cytoscape.slimscape.internal.RunSlimfinder;
 import org.cytoscape.util.swing.OpenBrowser;
 import org.cytoscape.view.model.CyNetworkViewFactory;
@@ -14,17 +15,10 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -157,7 +151,7 @@ public class SlimfinderRunPanel extends JPanel {
                     // Send request to the server for that page
                     String id = idTextArea.getText();
                     try {
-                        List<String> csvResults = PrepareResults(
+                        List<String> csvResults = CommonMethods.PrepareResults(
                                 ("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id + "&rest=main"));
                         if (csvResults == null) {
                             throwError(id);
@@ -173,16 +167,16 @@ public class SlimfinderRunPanel extends JPanel {
                     if (selected.size() > 0) {
                         RunSlimfinder slimfinder = new RunSlimfinder(network, selected, optionsPanel);
                         String url = slimfinder.getUrl();
-                        String id = getJobID(url).replaceAll("\\s+","");
+                        String id = CommonMethods.getJobID(url).replaceAll("\\s+","");
                         idTextArea.setText(id);
                         // Make sure the job is ready before analysis starts
-                        boolean ready = jobReady("http://rest.slimsuite.unsw.edu.au/check&jobid=" + id);
+                        boolean ready = CommonMethods.jobReady("http://rest.slimsuite.unsw.edu.au/check&jobid=" + id);
 
                         while (!ready) {
-                            ready = jobReady("http://rest.slimsuite.unsw.edu.au/check&jobid=" + id);
+                            ready = CommonMethods.jobReady("http://rest.slimsuite.unsw.edu.au/check&jobid=" + id);
                         }
                         try {
-                            List<String> csvResults = PrepareResults(
+                            List<String> csvResults = CommonMethods.PrepareResults(
                                     ("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id + "&rest=main"));
                             if (csvResults == null) {
                                 throwError(id);
@@ -199,216 +193,6 @@ public class SlimfinderRunPanel extends JPanel {
         });
     }
 
-    /**
-     * @desc attains and analyses main results from the Slimsuite server.
-     * @param url - The url where the results should be located.
-     * @return Map - A map of List<String> containing the contents of the csv and occ blocks.
-     *               If there is an error, null is returned.
-     */
-    private List<String> PrepareResults(String url) {
-        // Gets URL data
-        try {
-            URL website = new URL(url);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-
-            String inputLine;
-            String lineOne = in.readLine();
-
-            // There is an error in the results obtained
-            if (lineOne.startsWith("ERROR")) {
-                in.close();
-                return null;
-            } else {
-                List<String> csv = new ArrayList<String>();
-                csv.add(lineOne);
-                while ((inputLine = in.readLine()) != null) {
-                    StringBuilder builder = new StringBuilder();
-                    boolean inBraces = false;
-                    for (char x : inputLine.toCharArray()) {
-                        if (x == '{') {
-                            inBraces = true;
-                            builder.append(x);
-                        } else if (x == '}') {
-                            inBraces = false;
-                            builder.append(x);
-                        } else if (x == ',' && inBraces) {
-                            builder.append('|');
-                        } else {
-                            builder.append(x);
-                        }
-                    }
-
-                    String toReturn = builder.toString();
-                    csv.add(toReturn);
-                }
-                in.close();
-
-                return csv;
-            }
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
-    /**
-     * @desc - Gets the IDs of each node in the slimdb results page.
-     * @param url - the URL to search for results in
-     * @return - list of all the IDs found, as strings
-     */
-    private List<String> getNodeIds(String url) {
-
-        try {
-            URL website = new URL(url);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-
-            String inputLine;
-
-            List<String> nodeIds = new ArrayList<String>();
-            // Add each line with more than two connections
-            while ((inputLine = in.readLine()) != null) {
-                if (inputLine.startsWith(">")) {
-                    String[] line = inputLine.split(" ");
-                    nodeIds.add(line[0].substring(1));
-                }
-            }
-            in.close();
-            return nodeIds;
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex + " in function");
-            return null;
-        }
-
-    }
-
-    /**
-     * @desc attains and analyses the upc results from the Slimsuite server, so that graph edges can be added later.
-     * @param url - The url where the results should be located.
-     * @return Map - A map of List<String> containing the contents of the upc lines with >1 elements present.
-     *               If there is an error, null is returned.
-     */
-    private List<String> getUpcResults(String url) {
-
-        try {
-            URL website = new URL(url);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-
-            String inputLine;
-            String lineOne = in.readLine();
-
-            // There is an error in the results obtained
-            if (lineOne.startsWith("ERROR")) {
-                in.close();
-                return null;
-            } else {
-                in.readLine(); // Removes the second line which contains unnecessary data for our purposes
-                List<String> upc = new ArrayList<String>();
-                // Add each line with more than two connections
-                while ((inputLine = in.readLine()) != null) {
-                    String[] line = inputLine.split("\\t");
-                    if (Integer.parseInt(line[1]) > 1) {
-                        upc.add(line[3]);
-                    }
-                }
-                in.close();
-
-                return upc;
-            }
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex + " in function");
-            return null;
-        }
-    }
-
-    /**
-     * @desc Creates a csv-specific JTable from an input of comma separated strings.
-     * @param input - a List<String> consisting of a series of comma-separated lines.
-     * @return JTable - a table populated with the input elements.
-     */
-    private JTable createCsvTable (List<String> input) {
-        // Get column names from input
-        JTable table;
-        List<String> names = Arrays.asList(input.get(0).split(","));
-        List<String> abbreviated = names.subList(10, names.size()-5);
-        Object columnNames[] = new String[abbreviated.size()];
-        abbreviated.toArray(columnNames);
-
-        // Create table
-        DefaultTableModel model = new DefaultTableModel(null, columnNames);
-        table = new JTable(model);
-        table.setPreferredScrollableViewportSize(new Dimension(400, 700));
-        table.setFillsViewportHeight(true);
-
-        // Add a row in table for each element in the input
-        int lines = input.size();
-        for(int c=1; c<lines; c++) {
-            List<String> line = Arrays.asList(input.get(c).split(","));
-            List<String> abbreviate = line.subList(10, line.size()-5);
-            Object lineObject[] = new String[abbreviate.size()];
-            abbreviate.toArray(lineObject);
-            model.addRow(lineObject);
-        }
-
-        // Table formatting
-        table.setEnabled(false);
-        TableColumn column;
-        for (int i = 0; i < abbreviated.size(); i++) {
-            column = table.getColumnModel().getColumn(i);
-            if (i == 2) {
-                column.setMinWidth(100);
-            } else {
-                column.setMinWidth(50);
-            }
-        }
-        return table;
-    }
-
-    /**
-     * @desc Creates an occ-specific JTable from an input of comma separated strings.
-     * @param input - a List<String> consisting of a series of comma-separated lines.
-     * @return JTable - a table populated with the input elements.
-     */
-    private JTable createOccTable (List<String> input) {
-        JTable table;
-        List<String> names = Arrays.asList(input.get(0).split(","));
-        List<String> abbreviated = names.subList(3, names.size()-7);
-        Object columnNames[] = new String[abbreviated.size()]; // line 2
-        abbreviated.toArray(columnNames);
-
-        // Create table
-        DefaultTableModel model = new DefaultTableModel(null, columnNames);
-        table = new JTable(model);
-        table.setPreferredScrollableViewportSize(new Dimension(400, 700));
-        table.setFillsViewportHeight(true);
-
-        // Add a row in table for each element in the input
-        int lines = input.size();
-        for(int c=1; c<lines; c++) {
-            List<String> line = Arrays.asList(input.get(c).split(","));
-            List<String> abbreviate = line.subList(3, line.size()-7);
-            Object lineObject[] = new String[abbreviate.size()];
-            abbreviate.toArray(lineObject);
-            model.addRow(lineObject);
-        }
-
-        // Table formatting
-        table.setEnabled(false);
-        TableColumn column;
-        for (int i = 0; i < abbreviated.size(); i++) {
-            column = table.getColumnModel().getColumn(i);
-            column.setMinWidth(100);
-        }
-
-        return table;
-    }
 
     /**
      * @desc - creates the results panels and alters/creates the cytoscape network as required.
@@ -417,15 +201,15 @@ public class SlimfinderRunPanel extends JPanel {
      */
     private void displayResults(List<String> csvResults, final String id) {
         // Get OCC Results
-        List<String> occResults = PrepareResults(
+        List<String> occResults = CommonMethods.PrepareResults(
                 ("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id + "&rest=occ"));
 
         // Get list of all node IDs from slimdb
-        List<String> nodeIds = getNodeIds("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id
+        List<String> nodeIds = CommonMethods.getNodeIds("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id
                 + "&rest=slimdb");
 
         // Get graph edge data from upc outputs
-        List<String> upc = getUpcResults("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id
+        List<String> upc = CommonMethods.getUpcResults("http://rest.slimsuite.unsw.edu.au/retrieve&jobid=" + id
                 + "&rest=upc");
 
         // Create button to take users to the full results
@@ -440,8 +224,8 @@ public class SlimfinderRunPanel extends JPanel {
             }
         });
 
-        JTable csv = createCsvTable(csvResults);
-        JTable occ = createOccTable(occResults);
+        JTable csv = CommonMethods.createCsvTable(csvResults);
+        JTable occ = CommonMethods.createOccTable(occResults);
 
         List<String> occIds = new ArrayList<String>();
         for (int y=0; y<occ.getRowCount(); y++) {
@@ -469,69 +253,4 @@ public class SlimfinderRunPanel extends JPanel {
           //      "Opening the output page in a web browser.");
     }
 
-    /**
-     * @desc - gets the job id from a running SLiMSuite service, for later use.
-     * @param url - the URL to search for the ID in.
-     * @return - the ID, as a string.
-     */
-    private String getJobID (String url) {
-        try {
-            URL website = new URL(url);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-
-            String inputLine;
-            String lineOne = in.readLine();
-
-            // There is an error in the results obtained
-            if (lineOne.startsWith("ERROR")) {
-                in.close();
-                return null;
-            } else {
-                String id = null;
-                while ((inputLine = in.readLine()) != null) {
-                    if (inputLine.contains("### slimfinder job")) {
-                        id = inputLine.split("job")[1];
-                    }
-                }
-                in.close();
-                return id;
-            }
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex + " in function");
-            return null;
-        }
-    }
-
-    /**
-     * @desc - Determines whether the job is ready for analysis or still running
-     * @param url - the URL of the run
-     */
-    private boolean jobReady (String url) {
-        String lineOne;
-        try {
-            URL website = new URL(url);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-            lineOne = in.readLine();
-            if (lineOne.contains("Finished")) {
-                in.close();
-                return true;
-            } else {
-                in.close();
-                int option = JOptionPane.showConfirmDialog(null, "Run is currently: " + lineOne +
-                                ". Click Yes to check again, or No to stop checking. Please note you'll get errors if you try to"
-                                + "process this before the job is completed.", "Job Pending", JOptionPane.YES_NO_OPTION);
-                return option != JOptionPane.YES_OPTION;
-            }
-
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex);
-            return false;
-        }
-    }
 }
